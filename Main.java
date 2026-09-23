3151648569ec7e3759c7357c5120b42205fecde4
Tankun426 <tankuntankun4266@gmail.com>
2026-09-23 16:55:57 +0700

    new

--- Main.java
+++ Main.java
@@ -1,20 +1,23 @@
+
 import javax.swing.*;
 
 public class Main extends JFrame {
+
     public Main() {
         setTitle("Meteo");
         setSize(800, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        String text = JOptionPane.showInputDialog(this,"Enter the meteo you need.");
+        String text = JOptionPane.showInputDialog(this, "Enter the meteo you need.");
         int number = 5;
-        try{
+        try {
             number = Integer.parseInt(text);
-            if (number < 1){
+            if (number < 1) {
                 number = 5;
             }
-        }catch(Exception e){}
+        } catch (Exception e) {
+        }
 
         Scene scene = new Scene(number);
         add(scene);
--- Meteo.java
+++ Meteo.java
@@ -1,41 +1,44 @@
+
 import java.awt.*;
 import javax.swing.*;
 
 public class Meteo implements Runnable {
+
     private int x;
     private int y;
     private int dx;
     private int dy;
     private final int size = 45;
     private Image image;
-    private Image explosionImage;   
+    private Image explosionImage;
     private Scene owner;
     boolean alive = true;
     private boolean exploding = false;
 
     public Meteo(int x, int y, int dx, int dy, int imageNumber, Scene owner) {
-		this.x = x;
-		this.y = y;
-		this.dx = dx;
-		this.dy = dy;
-		this.owner = owner;
-		image = new ImageIcon("images/" + imageNumber + ".png")
-				.getImage().getScaledInstance(size, size, Image.SCALE_SMOOTH);
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.owner = owner;
+        image = new ImageIcon("images/" + imageNumber + ".png")
+                .getImage().getScaledInstance(size, size, Image.SCALE_SMOOTH);
 
         explosionImage = new ImageIcon("images/bomb.gif")
                 .getImage();
-	}
+    }
+
     public void start() {
         Thread thread = new Thread(this);
         thread.start();
     }
 
-    public void run(){
-        while (alive){
-            if (owner.getWidth()<= size || owner.getHeight() <= size){
-                try{
+    public void run() {
+        while (alive) {
+            if (owner.getWidth() <= size || owner.getHeight() <= size) {
+                try {
                     Thread.sleep(5);
-                }catch(Exception e){
+                } catch (Exception e) {
                     return;
                 }
                 continue;
@@ -54,47 +57,69 @@ public class Meteo implements Runnable {
                 owner.repaint();
                 continue;
             }
-        
+
             x = x + dx;
-			y = y + dy;
-            
-        
-			// ชนขอบแล้วสะท้อนกลับ
-			if (x <= 0 || x + size >= owner.getWidth()) {
-				dx = -dx;
-			}
-			if (y <= 0 || y + size >= owner.getHeight()) {
-				dy = -dy;
-			}
+            y = y + dy;
+
+            // ชนขอบแล้วสะท้อนกลับ
+            if (x <= 0 || x + size >= owner.getWidth()) {
+                dx = -dx;
+                if (dx > 0) {
+                    dx++;
+                } else {
+                    dx--;
+                }
+            }
+            if (y <= 0 || y + size >= owner.getHeight()) {
+                dy = -dy;
+                if (dy > 0) {
+                    dy++;
+                } else {
+                    dy--;
+                }
+            }
 
             owner.isColliding(this);
-        
 
             owner.repaint();
-			try {
-				Thread.sleep(5);
-			} catch (InterruptedException e) {
-				return;
-			}
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                return;
+            }
         }
     }
 
-    public void explode(){
+    public void explode() {
         exploding = true;
     }
 
-    public boolean isExploding(){
+    public boolean isExploding() {
         return exploding;
     }
 
-    public boolean isAlive(){
+    public boolean isAlive() {
         return alive;
     }
 
-    public int getX() {return x;}
-	public int getY() {return y;}
-	public int getSize() {return size;}
-	public Image getImage() {return image;}
-    public Image getExplosionImage() {return explosionImage;}
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public Image getExplosionImage() {
+        return explosionImage;
+    }
 
 }
